77c398b4c28ccff8c0d63e539778b6b820183343
2405-011-pw <walypa56@gmail.com>
2024-07-18 13:25:37 +0100

    handle change function

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,20 @@ function App() {
     author: "",
   });
 
-  async function handleClick() {
+  async function handleGet() {
     const allQuotes = await axios.get(
       "https://web-server-with-sql.onrender.com/messages"
     );
     const randomDataIndex = Math.floor(Math.random() * allQuotes.data.length);
     setData(allQuotes.data[randomDataIndex]);
+  }
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setNewQuoteInfo((prevNewQuoteInfo) => ({
+      ...prevNewQuoteInfo,
+      [name]: value,
+    }));
   }
 
   return (
@@ -23,11 +31,21 @@ function App() {
       <div className="quoteGetter">
         <p>{data && "'" + data.text + "'"}</p>
         <p>{data && data.author}</p>
-        <button onClick={handleClick}>Get Random Quote</button>
+        <button onClick={handleGet}>Get Random Quote</button>
       </div>
       <div className="quotePoster">
-        <input placeholder="Quote"></input>
-        <input placeholder="Author"></input>
+        <input
+          placeholder="Quote"
+          value={newQuoteInfo.text}
+          onChange={handleChange}
+          name="text"
+        ></input>
+        <input
+          placeholder="Author"
+          value={newQuoteInfo.author}
+          onChange={handleChange}
+          name="author"
+        ></input>
         <button>Post Quote</button>
       </div>
     </>
